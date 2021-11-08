b10383f2c52b8602c09dd79b2aa9ef2c6c38ab1a
Kanthorn <kanthorn.wo@rmuti.ac.th>
2021-11-08 22:20:52 +0700

    เเก้ไข่

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -12,7 +12,7 @@ render(){
 
         <img src={profilepic} alt="profile" className="profilepic"/>
         
-        <ReactTypingEffect  text ={['Hello Word !','Welcome To Portfolio ','By React Js']} speed={50} className="typingeffect" />
+        <ReactTypingEffect  text ={['เจี๊ยวเล็กเท่ามด','Welcome To Portfolio ','By React Js']} speed={50} className="typingeffect" />
 
 
 
